679d9fd0e7e13addba9d9e6bef83c5123c4892de
g-poulos <cs04480@uoi.gr>
2021-12-26 19:42:51 +0200

    Completed generate_dataset.c

--- generate_dataset.c
+++ generate_dataset.c
@@ -6,6 +6,8 @@
 
 #define RAND_LOW -1
 #define RAND_HIGH 1
+#define SET_SIZE 8000
+#define ERROR_PROPABILITY 0.1
 
 float generate_random_float() {
 	float scale = rand() / (float) RAND_MAX;
@@ -26,29 +28,50 @@ int select_category(float x1, float x2) {
 	}else {
 		return 4;
 	}
-	
 	return 0;
 }
 
+float propability(){
+	return ((float)rand() / (float) RAND_MAX);
+}
+
+int change_category(int current_category){
+	int new_category = (rand() % 5);
+	while(new_category == current_category){
+		new_category = (rand() % 5);
+	}
+	return new_category;
+}
+
 int generate_dataset() {
-	FILE* fp;
-	fp = fopen("./s1.txt", "w");
+	FILE* training_set_fp, *test_set_fp;
+	training_set_fp = fopen("./training_set.txt", "w");
+	test_set_fp = fopen("./test_set.txt", "w");
+
 	float x1;
 	float x2;
 	int category;
 	srand(time(0));
-	for (size_t i = 0; i < 80; i++)
+	for (int i = 0; i < SET_SIZE; i++)
 	{
 		x1 = generate_random_float();
 		x2 = generate_random_float();
 		category = select_category(x1, x2);
-		printf("%f,%f,%d\n", x1, x2, category);	
-		fprintf(fp, "%f,%f,%d\n", x1, x2, category);
+		if (i < 4000){
+			//printf("%d: %f,%f,%d\n", i, x1, x2, category);	
+			fprintf(training_set_fp, "%f,%f,%d\n", x1, x2, category);
+		}else{
+			if (propability() <= ERROR_PROPABILITY)
+			{
+				category = change_category(category);
+			}	
+			//printf("%d: %f,%f,%d\n", i, x1, x2, category);	
+			fprintf(test_set_fp, "%f,%f,%d\n", x1, x2, category);
+		}
+		
 	}
-	
-	
-	
-	fclose(fp);
+	fclose(training_set_fp);
+	fclose(test_set_fp);
 	return 0;
 }
 
