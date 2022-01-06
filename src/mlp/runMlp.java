b263ed82cd9d3d65cfc9a34060b9c8ce654567eb
g-poulos <cs04480@uoi.gr>
2022-01-06 19:05:25 +0200

    More cleanup + writing to file

--- src/mlp/runMlp.java
+++ src/mlp/runMlp.java
@@ -3,17 +3,22 @@ import java.util.ArrayList;
 public class runMlp {
 
     public static void main(String[] args) {
-        Mlp mlp = new Mlp(3);
-        mlp.D = 2;
-        mlp.H1 = 10;
-        mlp.H2 = 8;
-        mlp.H3 = 4;
-        mlp.K = 4;
-        mlp.hiddenLayerActivationFunction = "tanh";
+        int D = 2;
+        int H1 = 10;
+        int H2 = 8;
+        int H3 = 4;
+        int K = 4;
+        double LEARNING_RATE = 0.005;
+        int BATCH_SIZE = 1;
+        int MINIMUM_EPOCHS = 700;
+        double TERMINATION_THRESHOLD = 0.1;
+        String hiddenLayerActivationFunction = "tanh";
+
+        Mlp mlp = new Mlp(3, D, H1, H2, H3, K, LEARNING_RATE, BATCH_SIZE,
+                MINIMUM_EPOCHS, TERMINATION_THRESHOLD, hiddenLayerActivationFunction);
+
         ArrayList<Double[]> trainingSet = mlp.readFile("../../data/training_set.txt");
         mlp.initWeights();
-        //mlp.backprop(input, label);
-        //mlp.printLayerInfo();
         mlp.gradientDescent(trainingSet);
         mlp.testNetwork();
     }
