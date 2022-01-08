eb6084ccd3319319b77b8b9a27cb8f30dcc3a16b
g-poulos <cs04480@uoi.gr>
2022-01-08 17:12:43 +0200

    Best performance files + typo: mpl

--- src/mlp/runMlp.java
+++ src/mlp/runMlp.java
@@ -1,20 +1,18 @@
 public class runMlp {
 
     public static void main(String[] args) {
-        int numOfHiddenLayers = 2; // type "2" or "3"
+        int numOfHiddenLayers = 3; // type "2" or "3"
         int D = 2;
-        int H1 = 8;
-        int H2 = 4;
+        int H1 = 10;
+        int H2 = 10;
         int H3 = 8; // Ignored if numOfHiddenLayers == 2
         int K = 4;
         String hiddenLayerActivationFunction = "tanh"; //type "relu" or "tanh"
-        double LEARNING_RATE = 0.01;
-        int BATCH_SIZE = 40;
+        double LEARNING_RATE = 0.003;
+        int BATCH_SIZE = 1;
         int MINIMUM_EPOCHS = 700;
-        double TERMINATION_THRESHOLD = 0.1;
+        double TERMINATION_THRESHOLD = 0.01;
 
-        // 10 8 8
-        // 15 8 8
         Mlp mlp = new Mlp(numOfHiddenLayers, D, H1, H2, H3, K, LEARNING_RATE, BATCH_SIZE,
                 MINIMUM_EPOCHS, TERMINATION_THRESHOLD, hiddenLayerActivationFunction);
 
